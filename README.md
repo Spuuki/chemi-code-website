c679df8450773de98ea6990fb63ed77f646f1bb3
Spuuki <notstephano325@gmail.com>
2024-01-29 15:01:43 +0100

    coming soon

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Chemi Code Website
+
+## Common commands used
+
+1) To deploy it to the live website use:
+
+    ```
+    npm deploy
+    ```
+2) To run the website locally use:
+
+    ```
+    npm start
+    ```
+    
+**Note: be sure to be in the repository directory**
--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+import Logo from "./images/logo.webp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
       </header>
+
+      <body className="d-flex align-items-center justify-content-center vh-100 primary-background">
+        <div className="">
+          <img src={Logo} alt="" className="Logo"></img>
+          <h2 className="text">Website in development,</h2>
+          <h2 className="text">come back soon!</h2>
+        </div>
+      </body>
+
     </div>
+
   );
 }
 
