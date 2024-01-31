58b0ba7e39f6bef398c4ec3d0adf6a353fc7e2ac
Spuuki <notstephano325@gmail.com>
2024-01-31 15:49:43 +0100

    Fix undraggable image

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       </header>
 
       <body className="d-flex flex-column align-items-center justify-content-center primary-background min-vh-100">
-        <img src={Logo} alt="" className="Logo img-fluid"></img>
+        <img src={Logo} alt="" className="Logo img-fluid" draggable="false"></img>
         <h2 className="text my-3">Website in development,</h2>
         <h2 className="text">come back soon!</h2>
       </body>
